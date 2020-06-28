d62f9eadd579869a2122ed94f3a366e3a178c78d
SirMatters <perov.krll@gmail.com>
2020-06-28 17:50:07 +0300

    Add redux ./src/reducers - add template reducer
    <App/>
    - create template component
    
    ./src/index.ts
    - create store with imported reducer
    - apply thunk as middleware
    - wrap <App/> with provider components

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import reducers from './reducers';
 
-interface AppProps {
-  color: string;
-  size?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps> {
-  state = {
-    counter: 0,
-  };
-
-  count = (operation: boolean): void => {
-    this.setState((currState: AppState) => ({
-      counter: currState.counter + (operation ? 1 : -1),
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <button
-          onClick={() => {
-            this.count(true);
-          }}
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            this.count(false);
-          }}
-        >
-          Decrement
-        </button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App color='red' />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
